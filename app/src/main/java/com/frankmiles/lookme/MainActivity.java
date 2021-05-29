8b634586a2e78ec4179116d77c879268422f1eb0
FrankMiles <38081214+frankmiles@users.noreply.github.com>
2021-05-29 20:16:25 +0530

    Reconfigured build.gradle and AndroidManifest.xml to start project

---
+++ app/src/main/java/com/frankmiles/lookme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.frankmiles.lookme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.println(Log.ASSERT,"debug","Starting MainActivity");
+    }
+}
